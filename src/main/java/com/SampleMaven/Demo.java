608eeddc1824d6f820c004af9b5f2618038c9759
Nivedha <nivedha.v@mstsolutions.com>
2017-04-12 16:04:43 +0530

    New Project SampleMaven

---
+++ src/main/java/com/SampleMaven/Demo.java
@@ -0,0 +1,14 @@
+package com.SampleMaven;
+
+public class Demo {
+	
+	public boolean getbool()
+	{
+		return false;
+	}
+public static void main(String[] args)
+{
+Demo dd=new Demo();
+System.out.println(dd.getbool());
+}
+}
---
+++ src/main/java/com/SampleMaven/DemoTest.java
@@ -0,0 +1,16 @@
+package com.SampleMaven;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+public class DemoTest {
+	Demo d=new Demo();
+	@Test
+	public void ShouldReturnTrue() {
+	
+		System.out.print(d.getbool());
+	}
+
+}
